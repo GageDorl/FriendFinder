de4c96a71a58ab3cdd2dbf6db4b2d2d682c63a63
Gage D'Orlando <gagedorlando@gmail.com>
2019-11-07 18:48:03 -0500

    fixing port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 var htmlRoutes = require('./app/routing/htmlRoutes.js')
 var apiRoutes = require('./app/routing/apiRoutes.js')
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
